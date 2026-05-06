4d42eec7623b70cb9da3f00a12737acc2ddbe6fa
Vivek Rajbhar <rajvivek2071@gmail.com>
2026-05-06 12:53:51 +0530

    Update Fail is natural level 1.py

--- Fail is natural level 1.py
+++ Fail is natural level 1.py
@@ -1,32 +1,38 @@
 import pygame
-import time
 pygame.init()
 screen=pygame.display.set_mode((800,600))
+
 # color
 black=(0,0,0)
 white=(255,255,255)
 purple=(125,0,125)
+
 # character position
 head_x,head_y=50,450
 chest_x,chest_y=46,460
 left_leg_x,left_leg_y=49,480
 right_leg_x,right_leg_y=57,480
+
 # Movement
 left_right=5
 leg_movement=1
-# jump physics
+
+# jump physics variable
 gravity=0.8
 jump_power=-18
 y_velocity=0
 onground=True
 velocity=3
+
 # floor
 xfloor=800
 refloor_con=False
+
 # Text
 font=pygame.font.SysFont("None",48)
 run=True
 clock=pygame.time.Clock()
+
 # draw character
 def character():
     head=pygame.draw.rect(screen,black,(head_x,head_y,10,10))
@@ -35,6 +41,7 @@ def character():
     right_leg=pygame.draw.rect(screen,black,(right_leg_x,right_leg_y,3,20))
     right_circle=pygame.draw.circle(screen,black,(right_leg_x+1,right_leg_y+17),5)
     left_circle=pygame.draw.circle(screen,black,(left_leg_x+1,left_leg_y+17),5)
+
 # gravity+jump
 def grav():
     global head_y, chest_y, left_leg_y, right_leg_y,y_velocity,onground
@@ -50,11 +57,13 @@ def grav():
             head_y=450
             chest_y=460
             onground=True
+
 # for smooth shrink texture floor
 def xfloo():
     global xfloor
     if xfloor > 400:
         xfloor -= 40
+
 while run:
     screen.fill(white)
     for event in pygame.event.get():
@@ -69,34 +78,41 @@ while run:
                     onground=False
                     
     press=pygame.key.get_pressed()
+
     # right movement
     if press[pygame.K_RIGHT]:
         head_x+=left_right
         chest_x+=left_right
         left_leg_x+=left_right
         right_leg_x+=left_right
+
     # left movement  
     if press[pygame.K_LEFT]:
         head_x-=left_right
         chest_x-=left_right
         left_leg_x-=left_right
         right_leg_x-=left_right
+
     if right_leg_x>=700:
         text=font.render("You win!!!",True,black)
         screen.blit(text,(300,400))
+
     if right_leg_x>=720:
         run=False
     grav()
+
     if right_leg_x>350:
         xfloo()
         refloor_con=True
+
     if  refloor_con:
         refloor=pygame.draw.rect(screen,purple,(450,500,350,100))
+
     if right_leg_y>510:
         run=False
+        
     floor=pygame.draw.rect(screen,purple,(0,500,xfloor,100))
     character()
     pygame.display.flip()
     clock.tick(60)
 pygame.quit()
-        
